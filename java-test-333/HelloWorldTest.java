35742b2a518df313a181a6106a31a7487651aafb
dhpyun <dhpyun@dbinc.co.kr>
2026-03-09 14:24:09 +0900

    feat_java_test_333_spring_boot_test

---
+++ java-test-333/HelloWorldTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HelloWorldTest {
+    @Test
+    public void testOutput() {
+        assertEquals("hello world", "hello world");
+    }
+}
